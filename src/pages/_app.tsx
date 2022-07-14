b00d73ed5861ce0c3ddbffef5e6d7d4b5bb2bfdd
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-14 23:17:55 +0200

    feat: create variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app';
-import '@/styles/global.scss';
+import '@/styles/base/styles.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
